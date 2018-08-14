80744574aa9c3cd3a58ef400b0b61190181d75aa
Nobuhiro Moteki <nobuhiro.moteki@gmail.com>
2018-08-14 19:27:24 +0900

    Delete aggregate_gen.hpp

--- aggregate_gen.hpp
+++
@@ -1,6 +0,0 @@
-
-extern std::default_random_engine re;
-extern std::uniform_real_distribution<float> urf;
-
-Eigen::MatrixXf aggregate_gen_SA(const float& a, const int& num_sph, const float& kf, const float& Df, const float& tol);
-Eigen::MatrixXf aggregate_gen_CCA(const float& a, const int& num_sph_SA, const int& levels, const float& kf, const float& Df, const float& tol);
